e62a9acc255f97cb08b6fd96b335e9201d213c80
Davi Lima <davidlima35566@gmail.com>
2026-09-11 15:10:01 -0300

    feature: adiciona opção de carregar jogo

--- src/main/java/me/davidsir1/fireandtrigger/telas/ResultadoTela.java
+++ src/main/java/me/davidsir1/fireandtrigger/telas/ResultadoTela.java
@@ -9,6 +9,7 @@ public class ResultadoTela {
         MANTER,
         IR_PARA_INICIAL,
         IR_PARA_NOVO_JOGO,
+        IR_PARA_CARREGAR_JOGO,
         IR_PARA_JOGO,
         SAIR
     }
--- src/main/java/me/davidsir1/fireandtrigger/telas/TelaInicial.java
+++ src/main/java/me/davidsir1/fireandtrigger/telas/TelaInicial.java
@@ -1,6 +1,7 @@
 package me.davidsir1.fireandtrigger.telas;
 
 import java.util.Scanner;
+import me.davidsir1.fireandtrigger.ContextoJogo;
 
 /**
  *
@@ -8,6 +9,10 @@ import java.util.Scanner;
  */
 public class TelaInicial extends TelaBase {
 
+    public TelaInicial(ContextoJogo contexto) {
+        super(contexto);
+    }
+    
     @Override
     public ResultadoTela.Acao exibir() {
         Scanner leitura = new Scanner(System.in);
@@ -15,13 +20,16 @@ public class TelaInicial extends TelaBase {
         System.out.println("F I R E  A N D  T R I G G E R");
         System.out.println();
         System.out.println("1 - Começar Jogo");
-        System.out.println("2 - Sair");
+        System.out.println("2 - Carregar Jogo");
+        System.out.println("3 - Sair");
         System.out.print("Escolha uma Opção: ");
         int opcao = leitura.nextInt();
         
         if (opcao == 1) {
             return ResultadoTela.Acao.IR_PARA_NOVO_JOGO;
         } else if (opcao == 2) {
+            return ResultadoTela.Acao.IR_PARA_CARREGAR_JOGO;
+        } else if (opcao == 3) {
             return ResultadoTela.Acao.SAIR;
         } else {
             return ResultadoTela.Acao.MANTER;
