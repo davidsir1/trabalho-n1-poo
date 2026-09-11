7d9d6419933c5be01d8508d922540e3b04d8d30b
Davi Lima <davidlima35566@gmail.com>
2026-09-11 17:56:40 -0300

    feature: adiciona atributos e metodos para classe Inimigo

--- src/main/java/me/davidsir1/fireandtrigger/entidades/Inimigo.java
+++ src/main/java/me/davidsir1/fireandtrigger/entidades/Inimigo.java
@@ -4,6 +4,15 @@ package me.davidsir1.fireandtrigger.entidades;
  *
  * @author david
  */
-public class Inimigo {
+public class Inimigo extends Personagem {
+    private double recompensaExperiencia;
 
+    public Inimigo(String nome, int nivel, int vidaMaxima, int ataqueBase, int defesaBase, double experienciaRecompensa) {
+        super(nome, nivel, vidaMaxima, ataqueBase, defesaBase);
+        this.recompensaExperiencia = experienciaRecompensa;
+    }
+
+    public double getRecompensaExperiencia() {
+        return recompensaExperiencia;
+    }
 }
