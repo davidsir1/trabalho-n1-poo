c2caa4fdb0c58e9e7907ffd2d46351eddab2742a
Davi Lima <davidlima35566@gmail.com>
2026-09-18 23:19:12 -0300

    feature: implementação do método acaoAutomatica()
    Método responsável por realizar ação do inimigo no combate do jogo

--- src/main/java/me/davidsir1/fireandtrigger/entidades/Inimigo.java
+++ src/main/java/me/davidsir1/fireandtrigger/entidades/Inimigo.java
@@ -1,11 +1,14 @@
 package me.davidsir1.fireandtrigger.entidades;
 
+import java.util.Random;
+
 /**
  *
- * @author david
+ * @author matheus
  */
 public class Inimigo extends Personagem {
     private double recompensaExperiencia;
+    private boolean usouHabilidadeEspecial;
 
     public Inimigo(String nome, int nivel, int vidaMaxima, int ataqueBase, int defesaBase, double experienciaRecompensa) {
         super(nome, nivel, vidaMaxima, ataqueBase, defesaBase);
@@ -14,5 +17,25 @@ public class Inimigo extends Personagem {
 
     public double getRecompensaExperiencia() {
         return recompensaExperiencia;
+    }
+    
+    public boolean usouHablidadeEspecial() {
+        return usouHabilidadeEspecial;
+    }
+
+    /*
+    Realiza a ação do inimigo: 80% de chance de ataque comum e 20% de chance de golpe
+    especial.
+    */
+    public void acaoAutomatica(Jogador alvo) {
+        Random sorteio = new Random();
+        usouHabilidadeEspecial = sorteio.nextInt(100) >= 80;
+        
+        if (usouHabilidadeEspecial) {
+            int danoEspecial = ataqueBase + 3;
+            alvo.receberDano(danoEspecial);
+        } else {
+            atacar(alvo);
+        }
     }
 }
