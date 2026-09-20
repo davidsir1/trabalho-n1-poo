7299f7b3e2167275eddd7e53d546bebf63600674
Davi Lima <davidlima35566@gmail.com>
2026-09-20 10:50:28 -0300

    feature: implementação dos metodos carregarJogo e salvarJogo

--- src/main/java/me/davidsir1/fireandtrigger/ContextoJogo.java
+++ src/main/java/me/davidsir1/fireandtrigger/ContextoJogo.java
@@ -1,6 +1,8 @@
 package me.davidsir1.fireandtrigger;
 
 import me.davidsir1.fireandtrigger.entidades.Jogador;
+import me.davidsir1.fireandtrigger.utilidades.GerenciadorSaves;
+import me.davidsir1.fireandtrigger.utilidades.SaveData;
 
 /**
  *
@@ -14,6 +16,28 @@ public class ContextoJogo {
     public ContextoJogo() {
         jogador = null;
         nivelMasmorra = 1;
+    }
+    
+    public void salvarJogo() {
+        if (jogador == null) {
+            System.out.println("Não existe jogador para salvar.");
+            return;
+        }
+        
+        GerenciadorSaves.salvar(jogador, nivelMasmorra);
+    }
+    
+    public boolean carregarJogo(String arquivo) {
+        try {
+            SaveData dados = GerenciadorSaves.carregar(arquivo);
+            this.jogador = GerenciadorSaves.reconstruirJogador(dados);
+            this.nivelMasmorra = dados.getNivelMasmorra();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Não foi possível carregar o jogo.");
+            System.out.println("Detalhes: " + e.getMessage());
+            return false;
+        }
     }
 
     public int getNivelMasmorra() {
